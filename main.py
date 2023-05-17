348ea9fb2d54fbe4080fe7e8a66e118fcd24194c
DEBASHISH THAKUR <53852657+debashishthakur@users.noreply.github.com>
2023-05-18 00:33:24 +0530

    base contact and signin added

--- main.py
+++ main.py
@@ -68,9 +68,18 @@ def processImage(filename, operation):
 def home():
     return render_template('index.html')
 
+
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/signin')
+def signin():
+    return render_template('signin.html')
 
 @app.route('/history')
 def history():
