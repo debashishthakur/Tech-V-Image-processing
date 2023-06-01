e8f5839dd78c1c3f835ae4cddccd7116d3be993b
DEBASHISH THAKUR <53852657+debashishthakur@users.noreply.github.com>
2023-06-02 00:04:04 +0530

    Commit

--- main.py
+++ main.py
@@ -11,18 +11,19 @@ ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'webp'}
 app = Flask(__name__)
 
 oauth = OAuth(app)
+app.secret_key = 'super secret key'
 google = oauth.register(
     name = 'google',
-    client_id = '',
-    client_secret = '',
+    client_id = '859816165368-07cfe0f606bm4ncnh6nmn4jv0n4kif7h.apps.googleusercontent.com',
+    client_secret = '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',
     access_token_url = 'https://accounts.google.com/o/oauth2/token',
     access_token_params = None,
     authorize_url = 'https://accounts.google.com/o/oauth2/auth',
     authorize_params = None,
     api_base_url = 'https://www.googleapis.com/oauth2/v1/',
-    client_kwargs = {'scope' : 'openid profile email'},
+    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
+    client_kwargs = {'scope' : 'profile email'},
 )
-app.secret_key = 'super secret key'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 def allowed_file(filename):
@@ -49,33 +50,42 @@ def processImage(filename, operation):
             imgProcessed = cv2.medianBlur(img, 17)
             cv2.imwrite(f"static/{filename}", imgProcessed)
             return filename
-        case "4":
-            myimage_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-     
-            #Take S and remove any value that is less than half
-            s = myimage_hsv[:,:,1]
-            s = np.where(s < 127, 0, 1) # Any value below 127 will be excluded
- 
-            # We increase the brightness of the image and then mod by 255
-            v = (myimage_hsv[:,:,2] + 127) % 255
-            v = np.where(v > 127, 1, 0)  # Any value above 127 will be part of our mask
- 
-            # Combine our two masks based on S and V into a single "Foreground"
-            foreground = np.where(s+v > 0, 1, 0).astype(np.uint8)  #Casting back into 8bit integer
- 
-            background = np.where(foreground==0,255,0).astype(np.uint8) # Invert foreground to get background in uint8
-            background = cv2.cvtColor(background, cv2.COLOR_GRAY2BGR)  # Convert background back into BGR space
-            foreground=cv2.bitwise_and(img,img,mask=foreground) # Apply our foreground map to original image
-            # finalimage = background+foreground # Combine foreground and background
- 
-            imgProcessed = - background + foreground
+        
+        
+        case "5":
+            imgProcessed = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
             cv2.imwrite(f"static/{filename}", imgProcessed)
             return filename
         
-        case "5":
-            imgProcessed = cv2.medianBlur(img, 15)
+        case "6":
+            imgProcessed = cv2.cvtColor(img, cv2.COLOR_LRGB2LAB)
             cv2.imwrite(f"static/{filename}", imgProcessed)
-            return render_template('index2.html')
+            return filename
+        
+        case "4":
+            hh, ww = img.shape[:2]
+
+            # threshold on white
+            # Define lower and uppper limits
+            lower = np.array([200, 200, 200])
+            upper = np.array([255, 255, 255])
+
+            # Create mask to only select black
+            thresh = cv2.inRange(img, lower, upper)
+
+            # apply morphology
+            kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (20,20))
+            morph = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+
+            # invert morp image
+            mask = 255 - morph
+
+            # apply mask to image
+            imgProcessed = cv2.bitwise_and(img, img, mask=mask)
+
+            
+            cv2.imwrite(f"static/{filename}", imgProcessed)
+            return filename
 
     pass
     
@@ -84,7 +94,8 @@ def processImage(filename, operation):
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    email = dict(session).get('email', None)
+    return render_template('landing.html')
 
 @app.route('/login')
 def login():
@@ -95,26 +106,28 @@ def login():
 
 @app.route('/authorize')
 def authorize():
-    google = oauth.create_client('google')
+    
     token = google.authorize_access_token()
-    resp = google.get('userinfo')
+    resp = google.get('userinfo', token=token)
     resp.raise_for_status()
     user_info = resp.json()
     # do something with the token and profile
-    return redirect('/')
+    session['email'] = user_info['email']
+    return render_template('index.html')
 
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+
 
 @app.route('/contact', methods=['POST', 'GET'])
 def contact():
     return render_template('contact.html')
 
-@app.route('/slider')
-def slider():
-    return render_template('slider.html', slider_value=50)
+
 
 @app.route('/signin')
 def signin():
